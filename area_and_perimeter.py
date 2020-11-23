45b482f49862a9205bb47316978ba4cc6ff408ee
Ubuntu <ubuntu@ip-172-31-56-74.ec2.internal>
2020-11-23 20:52:38 +0000

    Area and Perimeter coding

---
+++ area_and_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created by: Roman Cernetchi
+# Created on: November 2020
+# This program calculating the area and perimeter of a rectangle
+#     with dimensions 5cm x 3cm
+
+
+def main():
+    print("if a rectangle has dimensions:")
+    print("5cm x 3cm")
+    print("")
+    print("Area is {}cm^2".format(5*3))
+    print("Perimeter is {}cm".format(2*(5+3)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by: Roman Cernetchi
+# Created on: November 2020
+# This program does basic math
+
+
+def main():
+    #this function deos basic math
+    print(9+2)
+    print("12-3={}".format(12-3))
+    print("12/3={}".format(12/3))
+    print("3+6/2={}".format(3+6/2))
+    print("3+6*2={}".format(3+6*2))
+    print("5+2**3={}".format(5+2**3))
+
+
+if __name__ == "__main__":
+    main()
